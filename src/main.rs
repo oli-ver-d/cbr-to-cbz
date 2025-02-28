8baadf39bff8e1ef89a5e9a37592784eb995d520
Oliver Dennis <oliver.d.333@proton.me>
2025-02-28 16:35:35 +0000

    Clap program

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,18 @@
 use cbr_to_cbz::{extract, compress};
+use clap::Parser;
+
+#[derive(Parser)]
+struct Args {
+    input: String,
+    output: String,
+}
 
 fn main() {
-    println!("CBR to CBZ.");
-    // set up a clap program that takes a string for in path and a string for out path
-    // call lib to extract and then compress
-    // return the out file path + success msg
+    let args = Args::parse();
 
-    // e
-    let cbr_path = "placeholder";
-    let out_path = "placeholder";
-
-    let extracted_path = extract::extract_cbr(cbr_path).unwrap();
-    compress::create_cbz(&extracted_path, out_path).unwrap();
+    println!("Extracting {}", &args.input);
+    let extracted_path = extract::extract_cbr(&args.input).unwrap();
+    println!("Compressing {}", &args.output);
+    compress::create_cbz(&extracted_path, &args.output).unwrap();
+    println!("Successfully converted: {} -> {}", &args.input, &args.output);
 }
